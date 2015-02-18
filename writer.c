3a5a4898ef8d0e33592743f42f8403c689bca2ef
BMDan <github@angelfaq.com>
2015-02-18 21:56:01 +0000

    Parameterize a bit more

--- writer.c
+++ writer.c
@@ -4,44 +4,50 @@
 #include <errno.h>
 #include <string.h>
 
-#define BLOCKSIZE (1*1024*1024) //in bytes
-#define FLUSHIT
+#define BLOCKSIZE ((long) (1*1024*1024)) //in bytes
+#define BENCH_ROUNDS (100) // nuber of times BLOCKSIZE bytes will be written (note: int)
+#define FLUSHIT // fflush() after each write?
+#define SLEEP_MILLIS (100) // Easier-to-read SLEEP_NANOS
+#define SLEEP_NANOS (SLEEP_MILLIS*1000*1000) // This is what is actually used.
+#define DEFAULT_TO_SENDFILE 1 // Use sendfile(2) by default?
+#define OUTFILE "deletemetempfile.file" //This should probably be made less silly.
 
 int main( int argc, char **argv ) {
 /*  struct timespec {
     time_t tv_sec;        // seconds
     long   tv_nsec;       // nanoseconds
   }; */
-  const struct timespec ms100 = { 0, 100000000 };
-  int i, sfrv;
+  const struct timespec sleep_interval = { 0, SLEEP_NANOS };
+  int i;
+  ssize_t sfrv;
   FILE *fp;
   FILE *rand;
   struct timespec now, then;
-  char use_sendfile = 1;
-  char buf[BLOCKSIZE];
+  char use_sendfile = DEFAULT_TO_SENDFILE;
+  unsigned char buf[BLOCKSIZE];
 
-  fp = fopen( "deletemetempfile.file", "w" );
+  fp = fopen( OUTFILE, "w" );
 
   if ( !fp ) {
-    fprintf( stderr, "Unable to open my temp file.  Dying.\n" );
+    fprintf( stderr, "Unable to open my temp file: %s.  Dying.\n", strerror(errno) );
     exit( 1 );
   }
 
   rand = fopen( "/dev/urandom", "r" );
 
   if ( !rand ) {
-    fprintf( stderr, "Unable to open urandom.  Dying.\n" );
+    fprintf( stderr, "Unable to open urandom: %s.  Dying.\n", strerror(errno) );
     exit( 1 );
   }
 
-  for ( i = 0; i < 100; i++ ) {
+  for ( i = 0; i < BENCH_ROUNDS; i++ ) {
     clock_gettime( CLOCK_MONOTONIC, &then );
     if ( use_sendfile ) {
       sfrv = sendfile( fileno(fp), fileno(rand), NULL, BLOCKSIZE );
     } else {
       sfrv = read( fileno(rand), buf, BLOCKSIZE );
       if ( sfrv != BLOCKSIZE ) {
-	//No real way to recover from this.
+	// No real way to recover from this.
 	fprintf( stderr, "Failed read() on rand: %s", strerror(errno) );
 	exit( 1 );
       }
@@ -68,7 +74,7 @@ int main( int argc, char **argv ) {
       printf( "\n" );
     }
 
-    nanosleep( &ms100, NULL );
+    nanosleep( &sleep_interval, NULL );
   }
 
   return 0;
